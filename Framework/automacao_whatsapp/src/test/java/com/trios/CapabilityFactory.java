d618bd6830d026634c0325310f5b1ea520251856
LeandroSoares89 <leandrosoarescosta@live.com>
2021-07-01 15:36:46 -0300

    add novo capabiliti e ajusetes no ct0066

--- Framework/automacao_whatsapp/src/test/java/com/trios/CapabilityFactory.java
+++ Framework/automacao_whatsapp/src/test/java/com/trios/CapabilityFactory.java
@@ -12,7 +12,7 @@ public class CapabilityFactory {
    public static AndroidDriver<?> driver;
 
 
-    public void setUp() throws MalformedURLException {
+    public void setUpA205g() throws MalformedURLException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("Whatsapp", "G:\\Documents\\WhatsApp.apk");
         desiredCapabilities.setCapability("deviceName", "SM_A205G");
@@ -25,5 +25,20 @@ public class CapabilityFactory {
         driver = new AndroidDriver<>(remoteUrl, desiredCapabilities);
     }
 
+    public void setUpEmuladorTeste() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("Whatsapp", "G:\\Documents\\WhatsApp.apk");
+        desiredCapabilities.setCapability("deviceName", "Teste");
+        desiredCapabilities.setCapability("uidid", "30.7.4-7453540");
+        desiredCapabilities.setCapability("authenticationName", "5554");
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("platformVersion", "11.0 (R) - API 30");
+        desiredCapabilities.setCapability( "emulatorVersion", "30.7.4-7453540");
+        desiredCapabilities.setCapability("ensureWebviewsHavePages", true);
+
+        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+        driver = new AndroidDriver<>(remoteUrl, desiredCapabilities);
+    }
 
 }
